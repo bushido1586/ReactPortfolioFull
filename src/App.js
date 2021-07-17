cff1ff5f72c03a1dcb112795df80d12a6e96fdf0
Bradley Reed <shenmusha@yahoo.com>
2021-07-17 19:14:35 -0400

    started home component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
